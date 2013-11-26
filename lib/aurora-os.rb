dea8c0eda6be6a75da901608cff4cacdee8ea831
eric paul <multipurposegeek@gmail.com>
2013-11-26 10:10:23 -0800

    Bump version number.

--- lib/aurora-os.rb
+++ lib/aurora-os.rb
@@ -7,6 +7,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('aurora-os', :path => extension_path)
 
 module Aurora_OS
-  VERSION = "0.0.1"
+  VERSION = "0.1.0"
   DATE = "2013-11-21"
 end
