7f67dd49406d64022c24cae8f23eb1a7a4ecfdf5
eric paul <multipurposegeek@gmail.com>
2013-11-25 16:19:56 -0800

    Rename library for naming conventions.

--- lib/aurora-os.rb
+++ lib/aurora-os.rb
@@ -4,7 +4,7 @@ require 'sassy-buttons'
 require 'compass-normalize'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('os-aurora', :path => extension_path)
+Compass::Frameworks.register('aurora-os', :path => extension_path)
 
 module Aurora_OS
   VERSION = "0.0.1"
