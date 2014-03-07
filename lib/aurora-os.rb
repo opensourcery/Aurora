6ea4b1a9b8a043850bffa2be56cbdb6928d01a94
Anne Sturdivant <anne@opensourcery.com>
2014-03-07 12:00:05 -0800

    changes for new release 0.2.3

--- lib/aurora-os.rb
+++ lib/aurora-os.rb
@@ -7,6 +7,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('aurora-os', :path => extension_path)
 
 module Aurora_OS
-  VERSION = "0.2.2"
-  DATE = "2014-02-13"
+  VERSION = "0.2.3"
+  DATE = "2014-03-07"
 end
